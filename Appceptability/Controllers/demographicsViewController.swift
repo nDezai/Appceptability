ae50827b5e5c1625062022e608b169d8bd443e0f
Neel Desai <neel.desai.13@ucl.ac.uk>
2020-02-25 10:20:26 +0000

    UI Switches added and functionality implemented

--- Appceptability/Controllers/demographicsViewController.swift
+++ Appceptability/Controllers/demographicsViewController.swift
@@ -17,9 +17,36 @@ class demographicsViewController: UIViewController {
     @IBOutlet weak var allAboveSwitch: UISwitch!
     @IBOutlet weak var saveAndContinueButton: UIButton!
     
+    override func viewWillAppear(_ animated: Bool) {
+        allAboveSwitch.isOn = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
+    @IBAction func switchOnePressed(_ sender: UISwitch) {
+        allAboveSwitch.setOn(!sender.isOn, animated: true)
+    }
+    
+    @IBAction func switchTwoPressed(_ sender: UISwitch) {
+        allAboveSwitch.setOn(!sender.isOn, animated: true)
+    }
+    
+    @IBAction func switchThreePressed(_ sender: UISwitch) {
+        allAboveSwitch.setOn(!sender.isOn, animated: true)
+    }
+    
+    @IBAction func switchFourPressed(_ sender: UISwitch) {
+        allAboveSwitch.setOn(!sender.isOn, animated: true)
+    }
+    
+    @IBAction func allSwitchPressed(_ sender: UISwitch) {
+        participantIDSwitch.setOn(!sender.isOn, animated: true)
+        sampleIDSwitch.setOn(!sender.isOn, animated: true)
+        ageSwitch.setOn(!sender.isOn, animated: true)
+        locationSwitch.setOn(!sender.isOn, animated: true)
+    }
+    
 }
